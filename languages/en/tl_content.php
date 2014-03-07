5a6355f4af29ebd5541f46818ae82f360d933f6c
delirius <github@delirius.ch>
2014-03-07 14:12:12 +0100

    Update tl_content.php

--- languages/en/tl_content.php
+++ languages/en/tl_content.php
@@ -24,4 +24,5 @@
 $GLOBALS['TL_LANG']['tl_content']['previewFile']	= array('Download file', 'Plese select the file you want to link for downloading.');
 $GLOBALS['TL_LANG']['tl_content']['previewImage']	= array('Preview image', 'You can set a custom preview image instead of the automatic generation.');
 $GLOBALS['TL_LANG']['tl_content']['previewTips']	= array('Show popup', 'Show more information on hover.');
+$GLOBALS['TL_LANG']['tl_content']['previewImageSize']	= array('Image width and height', 'Here you can set the image dimensions and the resize mode.');
 
